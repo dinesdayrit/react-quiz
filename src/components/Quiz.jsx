aab55d491dd2b3efd69f35bbbbb4d0155a7f9018
dinesdayrit <dayritdines22@gmail.com>
2024-03-14 14:03:16 +0800

    add timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,27 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer";
 
 export default function Quiz() {
     const [userAnsers, setUserAnswer] = useState([]);
 
-    const activeQuestionIndex = userAnsers.length
+    const activeQuestionIndex = userAnsers.length;
+    const quizIsComplete  = activeQuestionIndex === QUESTIONS.length;
+
+    if (quizIsComplete) {
+        return <div id="summary">
+        <img src={quizCompleteImg} alt="quizCompleteImg"/>
+        <h2>Quiz Complete</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() -0.5);
 
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAnswer((prevUserAnswer) => {
             return [...prevUserAnswer, selectedAnswer];
         });
+        
     }
 
     return (
         <div id="quiz">
         <div id="question">
+            <QuestionTimer
+             timeout={10000}
+             onTimeout={() => handleSelectAnswer(null)} 
+            />
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">
-                {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                {shuffledAnswers.map((answer) => (
                     <li key={answer} className="answer">
                      <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                     </li>
-                    
                 ))}
             </ul>
         </div>
