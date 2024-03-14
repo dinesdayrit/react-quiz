899f57dd06062b880341728035e034a5ae399c55
dinesdayrit <dayritdines22@gmail.com>
2024-03-14 13:08:52 +0800

    update quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react"
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnsers, setUserAnswer] = useState([]);
 
-    return <p>Currently Active Question</p>
+    const activeQuestionIndex = userAnsers.length
+
+    const handleSelectAnswer = (selectedAnswer) => {
+        setUserAnswer((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer];
+        });
+    }
+
+    return (
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    <li key={answer} className="answer">
+                     <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                    
+                ))}
+            </ul>
+        </div>
+    )
 }
